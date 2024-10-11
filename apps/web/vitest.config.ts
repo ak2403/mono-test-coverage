f1d77b6e66723284e4d4ccef8dd44d208d78bf26
Arun Kumar <arunpasupathi24@gmail.com>
2024-10-11 17:27:52 +1100

    feat: added first test

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -6,19 +5,7 @@ export default defineConfig({
     globals: true,
     include: ["**/*.test.*"],
     exclude: ["node_modules", "test/smoke"],
-    setupFiles: ["test/vitest/setup.ts"],
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
     coverage: {
-      exclude: [
-        "build/**",
-        "playwright.config.ts",
-        "next.config.js",
-        "deployment/bin/**",
-        ".next/**",
-        "playwright-test-results/**",
-      ],
       reporter: ["json-summary"],
     },
   },
