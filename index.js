07c3371469ef68939244e9ae83ae78ca3999db5e
Arun Kumar <arunpasupathi24@gmail.com>
2024-10-11 19:49:44 +1100

    chore: added more test for coverage

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ try {
   // 5. Save report to file
   const coverage = writeCoverageReportToFile(diffCoverageReport);
 
-  core.setOutput('coverage', coverageReportForVisualRepresentation);
+  core.setOutput('coverage', JSON.stringify(coverageReportForVisualRepresentation));
 } catch (error) {
   // Handle errors and indicate failure
   core.setFailed(error.message);
